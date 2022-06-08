c111d730e8e5fd467971d8a446e24cdb3691f549
aleksandr.martiushov <aleksandr.martiushov@signavio.com>
2022-06-08 15:46:03 +0200

    initial skeleton

---
+++ src/main/kotlin/io/mart/dbExperiments/DbExperimentsApplication.kt
@@ -0,0 +1,11 @@
+package io.mart.dbExperiments
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbExperimentsApplication
+
+fun main(args: Array<String>) {
+	runApplication<DbExperimentsApplication>(*args)
+}
---
+++ src/test/kotlin/io/mart/dbExperiments/DbExperimentsApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.mart.dbExperiments
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DbExperimentsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
